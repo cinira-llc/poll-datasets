8e2aa85393b780ec0d09b318ece134e961374876
Matt Jensen <mattj65816@gmail.com>
2024-04-16 10:44:46 -0500

    fix: testing.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import {EventBridgeHandler} from "aws-lambda";
+import {S3} from "aws-sdk";
 import _ from "lodash";
 
-export const handler: EventBridgeHandler<"Scheduled Event", any, string> = async (event) => {
-    const payload = _.assign({}, event, {
-        something: "else"
+const s3 = new S3({
+    apiVersion: "2006-03-01"
+});
+
+export const handler: EventBridgeHandler<"Scheduled Event", any, string> = async (event) =>
+    new Promise<string>((resolve, reject) => {
+        s3.listObjects({
+            Bucket: "cinira",
+            Prefix: "faa-cifp/CIFP_"
+        }, (err, data) => {
+            if (err) {
+                reject(err.message);
+            }
+            const keys = _.transform(data.Contents || [], (contents, {Key}) => {
+                contents.push(Key || "(unknown)");
+            }, [] as string[]);
+            resolve(JSON.stringify({
+                event, keys
+            }));
+        });
     });
-    return JSON.stringify(payload);
-}
